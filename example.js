b7fea7d076961d2f579b8dccb3659ea261942f48
Matt <m_dittmann@yahoo.com>
2017-04-17 23:52:59 -0700

    add click event to load the sub groups

--- example.js
+++ example.js
@@ -20,4 +20,25 @@ $(function() {
 
 	loadTimetable();
 
+	// CLICK ON THE EVENT TO LOAD A TIMETABLE
+	$('#content').on('click', '#event a', function(e) {
+		e.preventDefault();
+		var loc = this.id.toUpperCase();
+
+		var newContent = '';
+		for (var i = 0; i < times[loc].length; i++) {
+			newContent += '<li><span class="time">' + times[loc][i].time + '</span>';
+			newContent += '<a href="descriptions.html#';
+			newContent += times[loc][i].title.replace(/ /g, '-') + '">';
+			newContent += times[loc][i].title + '</a></li>';
+		}
+
+		$('#sessions').html('<ul>' + newContent + '</ul>');
+
+		$('#event a.current').removeClass('current');
+		$(this).addClass('current');
+
+		$('#details').text('');
+	});
+
 });
